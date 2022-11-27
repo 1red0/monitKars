15ce1106903bf9d507fe9bcd2ef9c8a07447343a
Andrei <andrei.rosu@empact.app>
2022-11-27 16:34:53 +0200

    Create repository for frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DefaultComponent } from './layouts/default/default.component';
+import { AccountComponent } from './modules/account/account.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { LoginComponent } from './modules/login/login.component';
+import { PostsComponent } from './modules/posts/posts.component';
+import { SearchComponent } from './modules/search/search.component';
+import { SettingsComponent } from './modules/settings/settings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: '', 
+    title: 'Dashboard', 
+    component: DefaultComponent, 
+    children: [{
+      path: '', 
+      component: DashboardComponent
+    }, {
+      path: 'posts',
+      component: PostsComponent
+    }, {
+      path: 'settings',
+      component: SettingsComponent
+    }, {
+      path: 'account',
+      component: AccountComponent
+    }, {
+      path: 'search',
+      component: SearchComponent
+    }, {
+      path: 'login', 
+      component: LoginComponent
+    }
+    
+  ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,46 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { DefaultModule } from './layouts/default/default.module';
+import { SettingsComponent } from './modules/settings/settings.component';
+import { AccountComponent } from './modules/account/account.component';
+import { SearchComponent } from './modules/search/search.component';
+import { LoginComponent } from './modules/login/login.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SettingsComponent,
+    AccountComponent,
+    SearchComponent,
+    LoginComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    DefaultModule
   ],
   providers: [],
   bootstrap: [AppComponent]
